546b6c24a4f45e3ad6c5869cc363cf47ed49f6b0
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-09-06 10:34:14 +0100

    add code to create required tables in seed file

--- seed.js
+++ seed.js
@@ -12,6 +12,38 @@ const seed = () => {
         .then(() => {
             return db.query(`DROP TABLE IF EXISTS equipment;`);
         })
+        .then(() => {
+            const equipmentTableQuery = db.query(`
+                CREATE TABLE equipment (
+                    equipment_id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL
+                );`);
+
+            const muscleGroupTableQuery = db.query(`
+                CREATE TABLE muscle_groups (
+                    group_id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL
+                );`);
+            
+            const exercisesTableQuery = db.query(`
+                CREATE TABLE exercises (
+                    exercise_id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    description TEXT,
+                    equipment_id INT,
+                    group_id INT,
+                    exercise_category TEXT,
+                    image_url TEXT,
+                    video_url TEXT,
+                    FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id) ON DELETE CASCADE,
+                    FOREIGN KEY (group_id) REFERENCES muscle_groups(group_id) ON DELETE CASCADE
+                );`);
+            
+            return Promise.all([equipmentTableQuery, muscleGroupTableQuery, exercisesTableQuery]);
+        })
+        .catch((err) => {
+            console.log("Error whilst running seed: ", err);
+        });
 };
 
 module.exports = seed;
