bb7dd9dd928bc5848525b6140848d6a7d7c503bd
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-09-06 16:42:55 +0100

    Set Up Express with first endpoint to fetch all exercises

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const { helloWorld, getExercises } = require('./controllers/exercises.controller');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/', helloWorld);
+
+app.get('/api/exercises', getExercises);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    const status = err.status || 500;
+    const message = err.msg || "Internal Server Error";
+    res.status(status).send({ msg: message });
 });
 
 module.exports = app;
